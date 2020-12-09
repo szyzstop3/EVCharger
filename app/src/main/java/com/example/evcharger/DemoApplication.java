05c83230f1f9b8a1cfdc805e1e3b44204da7a2c0
孙泽阳 <2018212939@bupt.edu.cn>
2020-12-09 16:25:08 +0800

    Second

--- app/src/main/java/com/example/evcharger/DemoApplication.java
+++ app/src/main/java/com/example/evcharger/DemoApplication.java
@@ -6,6 +6,7 @@ import com.baidu.mapapi.CoordType;
 import com.baidu.mapapi.SDKInitializer;
 
 public class DemoApplication extends Application {
+
     @Override
     public void onCreate() {
         super.onCreate();
