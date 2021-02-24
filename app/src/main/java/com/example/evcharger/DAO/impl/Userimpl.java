d168a21d3033a160ea3b452f5ee544ec3c1b96b0
孙泽阳 <2018212939@bupt.edu.cn>
2021-02-24 21:27:01 +0800

    云数据库1.0

---
+++ app/src/main/java/com/example/evcharger/DAO/impl/Userimpl.java
@@ -0,0 +1,34 @@
+package com.example.evcharger.DAO.impl;
+
+import com.example.evcharger.DAO.UserDAO;
+import com.example.evcharger.db.DBConnect;
+import com.example.evcharger.vo.User;
+
+import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Userimpl implements UserDAO {
+    @Override
+    public boolean InsertUser(User user) {
+        String sql="INSERT INTO `evcharger`.`user` (`Name`, `Phone`, `Password`,`Sex`) " +
+                "VALUES (?, ?, ?, ?);";
+        PreparedStatement PS;
+        DBConnect DBC=null;
+        try {
+            DBC = new DBConnect();
+            PS = DBC.getConnection().prepareStatement(sql);
+            PS.setString(1,user.getName());
+            PS.setString(2,user.getPhone());
+            PS.setString(3,user.getPassword());
+            PS.setString(4,""+user.getSex());
+            PS.executeUpdate();
+            PS.close();
+        } catch (IOException | SQLException throwables) {
+
+            throwables.printStackTrace();
+        }
+        DBC.close();
+        return false;
+    }
+}
--- app/src/main/java/com/example/evcharger/RegisterFragment.java
+++ app/src/main/java/com/example/evcharger/RegisterFragment.java
@@ -1,16 +1,13 @@
 package com.example.evcharger;
 
 import android.os.Bundle;
-
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 /**
  * A simple {@link Fragment} subclass.
