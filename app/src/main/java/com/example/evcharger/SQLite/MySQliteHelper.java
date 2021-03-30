d32e53175f098130162134e6667103b0991814c2
孙泽阳 <409167091@qq.com>
2021-03-31 00:52:26 +0800

    优化++++

---
+++ app/src/main/java/com/example/evcharger/SQLite/MySQliteHelper.java
@@ -0,0 +1,30 @@
+package com.example.evcharger.SQLite;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class MySQliteHelper extends SQLiteOpenHelper {
+    public static final String CREAT_USERLOGIN = "create table User(" +
+            "userid integer primary key autoincrement," +
+            "username varchar(36)," +
+            "pwd varchar(36))";
+    private Context mcontext;
+    public MySQliteHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+        this.mcontext = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(CREAT_USERLOGIN);
+        Toast.makeText(mcontext,"退出登陆成功！",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    }
+}
