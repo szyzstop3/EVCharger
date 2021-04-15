c67c1f3d1d4440fb9316aaa8159042ed2c74fe69
孙泽阳 <409167091@qq.com>
2021-04-15 23:55:21 +0800

    同步VO

--- app/src/main/java/com/example/evcharger/DAO/ToolDAO.java
+++ app/src/main/java/com/example/evcharger/DAO/ToolDAO.java
@@ -1,7 +1,5 @@
 package com.example.evcharger.DAO;
 
-import com.example.evcharger.vo.StationInfo;
-
 interface ToolDAO {
-    public StationInfo StationInfodiv(String scanString);
+
 }
--- app/src/main/java/com/example/evcharger/vo/StationInfo.java
+++
@@ -1,7 +0,0 @@
-package com.example.evcharger.vo;
-
-public class StationInfo {
-    private double Uprice;
-    private double Power;
-
-}
